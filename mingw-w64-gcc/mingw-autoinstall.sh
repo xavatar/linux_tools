c4e6059e8a60877736e90669078b188af5cb0c11
Tatar Xavatar <perrinx@outlook.fr>
2020-03-21 14:58:06 +0100

    Enable AUR

--- mingw-w64-gcc/mingw-autoinstall.sh
+++ mingw-w64-gcc/mingw-autoinstall.sh
@@ -2,6 +2,10 @@
 
 # Created by: Xavatar <xavatar at outlook dot fr>
 
+# Enable AUR
+sudo sed --in-place "s/#EnableAUR/EnableAUR/" "/etc/pamac.conf"
+
+# Install mingw
 pamac install mingw-w64-crt-bin --no-confirm
 pamac install mingw-w64-binutils-bin --no-confirm
 pamac install mingw-w64-winpthreads-bin --no-confirm
