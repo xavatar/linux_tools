1836e252755a2d1dcf9a0f0b4d28c44f064bf222
Tatar Xavatar <perrinx@outlook.fr>
2020-03-21 15:08:24 +0100

    Remove no confirm

--- mingw-w64-gcc/mingw-autoinstall.sh
+++ mingw-w64-gcc/mingw-autoinstall.sh
@@ -6,8 +6,8 @@
 sudo sed --in-place "s/#EnableAUR/EnableAUR/" "/etc/pamac.conf"
 
 # Install mingw
-pamac install mingw-w64-crt-bin --no-confirm
-pamac install mingw-w64-binutils-bin --no-confirm
-pamac install mingw-w64-winpthreads-bin --no-confirm
-pamac install mingw-w64-headers-bin --no-confirm
-pamac install mingw-w64-gcc-bin --no-confirm
+pamac install mingw-w64-crt-bin
+pamac install mingw-w64-binutils-bin
+pamac install mingw-w64-winpthreads-bin
+pamac install mingw-w64-headers-bin
+pamac install mingw-w64-gcc-bin
