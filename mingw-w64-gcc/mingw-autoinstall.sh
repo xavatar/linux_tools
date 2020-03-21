47af3c5414bf8994a08c3f80e9f165c75134c320
Tatar Xavatar <perrinx@outlook.fr>
2020-03-21 14:51:20 +0100

    First Creation

---
+++ mingw-w64-gcc/mingw-autoinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Created by: Xavatar <xavatar at outlook dot fr>
+
+pamac install --no-confirm mingw-w64-crt-bin
+pamac install --no-confirm mingw-w64-binutils-bin
+pamac install --no-confirm mingw-w64-winpthreads-bin
+pamac install --no-confirm mingw-w64-headers-bin
+pamac install --no-confirm mingw-w64-gcc-bin
