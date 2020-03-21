62733ea2e7d38780a5da9ccdae579a1475c67a74
Tatar Xavatar <perrinx@outlook.fr>
2020-03-21 14:54:49 +0100

    Update mingw-autoinstall.sh

--- mingw-w64-gcc/mingw-autoinstall.sh
+++ mingw-w64-gcc/mingw-autoinstall.sh
@@ -2,8 +2,8 @@
 
 # Created by: Xavatar <xavatar at outlook dot fr>
 
-pamac install --no-confirm mingw-w64-crt-bin
-pamac install --no-confirm mingw-w64-binutils-bin
-pamac install --no-confirm mingw-w64-winpthreads-bin
-pamac install --no-confirm mingw-w64-headers-bin
-pamac install --no-confirm mingw-w64-gcc-bin
+pamac install mingw-w64-crt-bin --no-confirm
+pamac install mingw-w64-binutils-bin --no-confirm
+pamac install mingw-w64-winpthreads-bin --no-confirm
+pamac install mingw-w64-headers-bin --no-confirm
+pamac install mingw-w64-gcc-bin --no-confirm
