6bb3cb02ef9893325d8b4a5d5bb7953254a5044d
Tatar Xavatar <perrinx@outlook.fr>
2020-03-23 22:20:33 +0100

    typo

--- script/dep-compil-faudio.sh
+++ script/dep-compil-faudio.sh
@@ -13,13 +13,11 @@ pamac install lib32-glslang
 pamac install lib32-shaderc
 pamac install lib32-libplacebo
 pamac install lib32-dav1d
-
 pamac install lib32-libomxil-bellagio
 pamac install lib32-opencore-amr
 pamac install lib32-openjpeg2
-pamac install lib32-x264>=159
+pamac install lib32-x264
 pamac install lib32-x265
 pamac install lib32-xvidcore
-
 pamac install lib32-ffmpeg
 
